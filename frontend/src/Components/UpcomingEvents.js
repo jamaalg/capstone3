ce72efb904bc9a0702ed9d0591bb00c40679361f
Jamaal Glanton <jamaal1087@gmail.com>
2023-08-30 09:19:00 -0400

    remove unused code, styling of page tweaks

--- frontend/src/Components/UpcomingEvents.js
+++ frontend/src/Components/UpcomingEvents.js
@@ -1,15 +1,7 @@
 import './Styles/UpcomingEvents.css';
 import { EventCard } from './EventCard.js';
-import { useState } from 'react';
 
 export const UpcomingEvents = ({ events }) => {
-  let yourDate = new Date();
-
-  const [date, setDate] = useState(yourDate.toISOString().split('T')[0]);
-
-  const sortedActivities = events.slice().sort((a, b) => b.date - a.date);
-
-  console.log({ sorted: sortedActivities });
   return (
     <div className='upcoming-events-container'>
       <h1 className='upcoming-events-header'>Upcoming Events</h1>
